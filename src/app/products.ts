b94f38fe29f5de94ed4871286758df90c0da8c62
masuda <masuda_naoki1016@hotmail.com>
2021-06-15 08:46:45 +0900

    商品ページ

---
+++ src/app/products.ts
@@ -0,0 +1,30 @@
+export const products = [
+    {
+      name: 'Phone XL',
+      price: 799,
+      description: 'A large phone with one of the best screens',
+      coverImage: './assets/img/phone-cover.jpg',
+      heading: 'A large phone with one of the best screens Phone XL',
+    },
+    {
+      name: 'Phone Mini',
+      price: 699,
+      description: 'A great phone with one of the best cameras',
+      coverImage: './assets/img/phone-cover.jpg',
+      heading: 'A great phone with one of the best cameras Phone Mini',
+    },
+    {
+      name: 'Phone Standard',
+      price: 299,
+      description: '',
+      coverImage: './assets/img/phone-cover.jpg',
+      heading: 'A great phone with one of the best cameras Phone Standard',
+    },
+    {
+      name: 'Phone Special',
+      price: 999,
+      description: '',
+      coverImage: './assets/img/phone-cover.jpg',
+      heading: 'A great phone with one of the best cameras Phone Special',
+    }
+  ];
