3d03426c9f39183ac5f82d210cb5674e6a3001f1
masuda <masuda_naoki1016@hotmail.com>
2021-06-23 06:48:30 +0900

    frontend-backend

--- src/app/product/product-detail/product-detail.component.ts
+++ src/app/product/product-detail/product-detail.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { products } from '../../products';
+import { ProductService } from '../shared/product.service';
 
 @Component({
   selector: 'app-product-detail',
@@ -9,11 +9,19 @@ import { products } from '../../products';
 })
 export class ProductDetailComponent implements OnInit {
   product: any;
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute,
+              private productService: ProductService) { }
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe(param=>{
-      this.product = products[+param.get('productId')!]
+    this.route.paramMap.subscribe((params) => {
+      const productObserval = this.productService.getProductById(params.get('productId')!);
+      productObserval.subscribe(
+        (data) => {
+          this.product = data;
+        },
+        (err) => { console.error('something wrong occurred: ' + err); },
+        () => { console.log('done'); }
+      );
     })
   }
 }
--- src/app/product/product-list/product-listings.component.ts
+++ src/app/product/product-list/product-listings.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { products } from 'src/app/products';
+import { ProductService } from '../shared/product.service';
+
 
 @Component({
   selector: 'app-product-listings',
@@ -8,10 +9,37 @@ import { products } from 'src/app/products';
 })
 export class ProductListComponent implements OnInit {
   products :any;
-  constructor() { }
+  constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
-    this.products = products;
-  }
+    // this.products = this.productService.getProducts();
+    const productObserval = this.productService.getProducts();
+    productObserval.subscribe(
+      (data) => {
+        this.products = data;
+      },
+      (err) => { console.error('something wrong occurred: ' + err); },
+      () => { console.log('done'); }
+    );
 
+    // const observable = new Observable(subscriber => {
+    //   subscriber.next(1);
+    //   subscriber.next(2);
+    //   subscriber.next(3);
+    //   setTimeout(() => {
+    //     subscriber.next(4);
+    //     subscriber.complete();
+    //   }, 1000);
+    // });
+
+    // debugger
+    // console.log('just before subscribe');
+    // debugger
+    // observable.subscribe({
+    //   next(x) { console.log('got value ' + x); },
+    //   error(err) { console.error('something wrong occurred: ' + err); },
+    //   complete() { console.log('done'); }
+    // });
+    // console.log('just after subscribe');
+  }
 }
