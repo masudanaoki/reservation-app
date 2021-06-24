cfee3ea0a93fe46dd6c35a938f10bbbaad476165
masuda <masuda_naoki1016@hotmail.com>
2021-06-25 08:02:31 +0900

    heroku

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const config = require('./config/dev')
+const config = require('./config')
 const FakeDb = require('./fake-db')
 const productRouter = require('./routes/product')
+const path = require('path')
 
 mongoose.connect(config.DB_URL, {
   useNewUrlParser: true,
@@ -11,13 +12,23 @@ mongoose.connect(config.DB_URL, {
   useCreateIndex: true
 }).then(
     () => {
-        const fakedb = new FakeDb()
-        fakedb.initDb()
+        if (process.env.NODE_ENV !== 'production') {
+            const fakedb = new FakeDb()
+            // fakedb.initDb()
+        }
     }
 );
 
 const app = express()
 app.use('/api/v1/product', productRouter)
+
+if (process.env.NODE_ENV === 'production') {
+    const appPath = path.join( __dirname, '..', 'dist', 'reservation-app')
+    app.use(express.static(appPath))
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(appPath, 'index.html'))
+    })
+}
 
 port = process.env.port || '3001'
 app.listen(port, () => {
